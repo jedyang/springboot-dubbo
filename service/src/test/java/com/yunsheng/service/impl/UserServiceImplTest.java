36034a712bc62e7a3d4b6622eef7c6ade89d31ae
杨云胜 <yangyunsheng@haier.com>
2018-07-30 17:57:31 +0800

    update
    配置替换

--- service/src/test/java/com/yunsheng/service/impl/UserServiceImplTest.java
+++ service/src/test/java/com/yunsheng/service/impl/UserServiceImplTest.java
@@ -6,13 +6,24 @@ import com.yunsheng.service.UserService;
 
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 
 import static org.junit.Assert.*;
 
 public class UserServiceImplTest extends ServiceApplicationTests{
 
+    @Value("${config.datasource.url}")
+    private String url;
+
+
     @Autowired
     private UserService userService;
+
+
+    @Test
+    public void testVal(){
+        System.out.println(url);
+    }
 
     @Test
     public void addUserRollBack() throws Exception {
@@ -22,6 +33,17 @@ public class UserServiceImplTest extends ServiceApplicationTests{
         a.setPhone("2345");
         a.setUserName("rb");
         userService.addUserRollBack(a);
+    }
+
+    @Test
+    public void addUser() throws Exception {
+        User a = new User();
+        java.util.Random r=new java.util.Random();
+        a.setUserId(r.nextInt(1000));
+        a.setPassword("123");
+        a.setPhone("2345");
+        a.setUserName("addOne");
+        userService.addUser(a);
     }
 
 }
